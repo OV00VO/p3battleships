d88a17ce0d430361e9ddc07f4e8451c3395b5f19
OV00VO <136384344+OV00VO@users.noreply.github.com>
2023-11-14 12:17:16 +0100

    Update run.py
    Test connection CA

--- run.py
+++ run.py
@@ -1,2 +1,13 @@
+import random
+import datetime
+
 name = input("What is your name? ")
 print("Hello World dear", name + "!")
+
+def test_system():
+  """Tests the system by printing out the current time and date."""
+  now = datetime.datetime.now()
+  print("The current time and date is:", now)
+
+if __name__ == "__main__":
+  test_system()
