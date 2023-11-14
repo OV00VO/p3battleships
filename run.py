86d728aca116caa064260e4d2536a2ceedb819ea
OV00VO <>
2023-11-14 12:05:41 +0000

    Add structure Battleship and Data Model

--- run.py
+++ run.py
@@ -1,13 +1,23 @@
 import random
-import datetime
 
-name = input("What is your name? ")
-print("Hello World dear", name + "!")
+MESSAGE_LENGTH = 80
 
-def test_system():
-  """Tests the system by printing out the current time and date."""
-  now = datetime.datetime.now()
-  print("The current time and date is:", now)
+class Board:
+    def __init__(self, size):
+        self.size = size
+        self.cells = [['_' for _ in range(size)] for _ in range(size)]
+        self.ships_remaining = 0
+      
+      def place_ship(self, ship_length, orientation):
+        while True:
+            start_row = random.randint(0, self.size - 1)
+            start_col = random.randint(0, self.size - 1)
 
-if __name__ == "__main__":
-  test_system()
+            if self.check_valid_ship_placement(start_row, start_col, orientation, ship_length):
+                for i in range(ship_length):
+                    if orientation == 'H':
+                        self.cells[start_row][start_col + i] = '#'
+                    else:
+                        self.cells[start_row + i][start_col] = '#'
+                self.ships_remaining += 1
+                break
